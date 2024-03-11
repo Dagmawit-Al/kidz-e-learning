fca17d2f924576dbe55cd6449a12033a5863ecdd
Tigabu <tigabu.m@productera.io>
2024-03-11 19:10:14 +0300

    feat: added single book page(#12)

--- src/components/Details/BlogsDetail.js
+++ src/components/Details/BlogsDetail.js
@@ -1,78 +1,77 @@
-import {
-  Tabs,
-  TabsHeader,
-  TabsBody,
-  Tab,
-  TabPanel,
-} from "@material-tailwind/react";
-import React, { useEffect } from "react";
+import { Typography, Card, CardBody } from "@material-tailwind/react";
 
-export function TabsDefault() {
-  const data = [
-    {
-      label: "HTML",
-      value: "html",
-      desc: `It really matters and then like it really doesn't matter.
-        What matters is the people who are sparked by it. And the people 
-        who are like offended by it, it doesn't matter.`,
-    },
-    {
-      label: "React",
-      value: "react",
-      desc: `Because it's about motivating the doers. Because I'm here
-        to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-    {
-      label: "Vue",
-      value: "vue",
-      desc: `We're not always in the position that we want to be at.
-        We're constantly growing. We're constantly making mistakes. We're
-        constantly trying to express ourselves and actualize our dreams.`,
-    },
-    {
-      label: "Angular",
-      value: "angular",
-      desc: `Because it's about motivating the doers. Because I'm here
-        to follow my dreams and inspire other people to follow their dreams, too.`,
-    },
-    {
-      label: "Svelte",
-      value: "svelte",
-      desc: `We're not always in the position that we want to be at.
-        We're constantly growing. We're constantly making mistakes. We're
-        constantly trying to express ourselves and actualize our dreams.`,
-    },
-  ];
-
+function ContentCard({ img, title, desc }) {
   return (
-    <Tabs value="html">
-      <TabsHeader className="bg-background">
-        {data.map(({ label, value }) => (
-          <Tab key={value} value={value} className=" m-4">
-            {label}
-          </Tab>
-        ))}
-      </TabsHeader>
-      <TabsBody>
-        {data.map(({ value, desc }) => (
-          <TabPanel key={value} value={value}>
-            {desc}
-          </TabPanel>
-        ))}
-      </TabsBody>
-    </Tabs>
+    <Card
+      className="relative grid min-h-[30rem] items-end overflow-hidden rounded-xl"
+      color="transparent"
+    >
+      <img
+        src={img}
+        alt="bg"
+        className="absolute inset-0 h-full w-full object-cover object-center"
+      />
+      <div className="absolute inset-0 bg-black/70" />
+      <CardBody className="relative flex flex-col justify-end">
+        <Typography variant="h4" color="white">
+          {title}
+        </Typography>
+        <Typography
+          variant="paragraph"
+          color="white"
+          className="my-2 font-normal"
+        >
+          {desc}
+        </Typography>
+      </CardBody>
+    </Card>
   );
 }
 
-const BlogDetail = () => {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <div>
-      <TabsDefault />
-    </div>
-  );
-};
+const contents = [
+  {
+    img: "https://www.material-tailwind.com/image/blog-11.jpeg",
+    title: "Search and Discovery",
+    desc: "Website visitors today demand a frictionless user expericence — especially when using search. Because of the hight standards we tend to offer.",
+  },
+  {
+    img: "https://www.material-tailwind.com/image/blog-10.jpeg",
+    title: "Last visits in US",
+    desc: "Wealth creation is an evolutionarily recent positive-sum game. Status is an old zero-sum game. Those attacking wealth creation are often just seeking status.",
+  },
+  {
+    img: "https://demos.creative-tim.com/material-kit-pro/assets/img/examples/card-blog2.jpg",
+    title: "Grow in a beautiful area",
+    desc: "Free people make free choices. Free choices mean you get unequal outcomes. You can have freedom, or you can have equal outcomes. You can't have both.",
+  },
+];
 
-export default BlogDetail;
+export function BlogsDetail() {
+  return (
+    <section className="container mx-auto px-8 py-10 lg:py-28">
+      <Typography
+        variant="h2"
+        color="blue-gray"
+        className="!text-2xl !leading-snug lg:!text-3xl"
+      >
+        Build something great
+      </Typography>
+      <Typography
+        variant="lead"
+        className="mt-2 max-w-lg !font-normal !text-gray-500"
+      >
+        We&apos;re constantly trying to express ourselves and actualize our
+        dreams. If you have the opportunity to play this game of life you need
+        to appreciate every moment.
+      </Typography>
+
+      <div className="mt-10 grid grid-cols-1 gap-10 lg:grid-cols-3">
+        {contents.map(({ img, title, desc }) => (
+          <ContentCard key={title} img={img} title={title} desc={desc} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default BlogsDetail;
